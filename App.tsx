377f06d42f6a74a1c8ae1f887bad827b6605cdde
Alfonso <alfonsolasaguirre@gmail.com>
2021-01-07 01:19:29 +0100

    Add swipper transition to each item

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Animated, {
   useAnimatedScrollHandler,
@@ -10,6 +10,7 @@ import { items } from "./Model";
 
 export default function App() {
   const y = useSharedValue(0);
+  const [scrollItems, setScrollItems] = useState(items);
 
   const onScroll = useAnimatedScrollHandler({
     onScroll: ({ contentOffset: { y: value } }) => {
@@ -22,11 +23,27 @@ export default function App() {
       <Animated.ScrollView
         onScroll={onScroll}
         scrollEventThrottle={16}
-        contentContainerStyle={{ height: (items.length + 1) * ITEM_MAX_HEIGHT }}
+        contentContainerStyle={{
+          height: (scrollItems.length + 1) * ITEM_MAX_HEIGHT,
+        }}
         decelerationRate="fast"
+        showsVerticalScrollIndicator={false}
       >
-        {items.map((item, index) => {
-          return <Item key={index} item={item} y={y} index={index} />;
+        {scrollItems.map((item, index) => {
+          return (
+            <Item
+              key={item.id}
+              item={item}
+              y={y}
+              index={index}
+              onDelete={(item) => {
+                const filterItems = scrollItems.filter(
+                  ({ id }) => id != item.id
+                );
+                setScrollItems(filterItems);
+              }}
+            />
+          );
         })}
       </Animated.ScrollView>
     </View>
--- components/Item/index.tsx
+++ components/Item/index.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { Image, Text, Dimensions } from "react-native";
+import {
+  Image,
+  Text,
+  Dimensions,
+  View,
+  StyleSheet,
+  Pressable,
+  Alert,
+  Button,
+} from "react-native";
+import {
+  PanGestureHandler,
+  PanGestureHandlerEventExtra,
+  PanGestureHandlerGestureEvent,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 import Animated, {
   Extrapolate,
   interpolate,
+  runOnJS,
+  useAnimatedGestureHandler,
   useAnimatedStyle,
+  useSharedValue,
+  withSpring,
 } from "react-native-reanimated";
+import { snapPoint } from "react-native-redash";
 
 import styles from "./styles";
 
@@ -14,6 +35,7 @@ export const ITEM_MAX_HEIGHT = ScreenHeight / 2;
 export const ITEM_MIN_HEIGHT = 128;
 
 interface Item {
+  id: number;
   title: string;
   subtitle: string;
   picture: number;
@@ -23,20 +45,18 @@ interface ItemProps {
   y: Animated.SharedValue<number>;
   index: number;
   item: Item;
+  onDelete?: (item: Item) => void;
 }
 
-const Item = ({ y, index, item }: ItemProps) => {
+const Item = ({ y, index, item, onDelete }: ItemProps) => {
+  const translateX = useSharedValue(0);
+  const leftFullSwipe = ScreenWidth;
+  const editWidth = 120;
+  const deleteWidth = 120;
+  // from left to right
+  const snapPoints = [-editWidth, 0, deleteWidth, leftFullSwipe];
   const { picture, title, subtitle } = item;
   const inputRange = [(index - 1) * ITEM_MAX_HEIGHT, index * ITEM_MAX_HEIGHT];
-
-  const containerStyle = useAnimatedStyle(() => ({
-    height: interpolate(
-      y.value,
-      inputRange,
-      [ITEM_MIN_HEIGHT, ITEM_MAX_HEIGHT],
-      Extrapolate.CLAMP
-    ),
-  }));
 
   const TextContainer = useAnimatedStyle(() => ({
     opacity: interpolate(y.value, inputRange, [0, 1], Extrapolate.CLAMP),
@@ -52,14 +72,149 @@ const Item = ({ y, index, item }: ItemProps) => {
     ],
   }));
 
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    { x: number }
+  >({
+    onStart: (_, ctx) => {
+      ctx.x = translateX.value;
+    },
+    onActive: ({ translationX }, ctx) => {
+      translateX.value = translationX + ctx.x;
+    },
+    onEnd: ({ velocityX }) => {
+      const dest = snapPoint(translateX.value, velocityX, snapPoints);
+      translateX.value = withSpring(
+        dest,
+        {
+          overshootClamping: true,
+        },
+        () => {
+          if (translateX.value === leftFullSwipe && onDelete) {
+            // return onDelete(item);
+            runOnJS(onDelete)(item);
+          }
+        }
+      );
+    },
+  });
+
+  const containerStyle = useAnimatedStyle(() => ({
+    height: interpolate(
+      y.value,
+      inputRange,
+      [ITEM_MIN_HEIGHT, ITEM_MAX_HEIGHT],
+      Extrapolate.CLAMP
+    ),
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+    ],
+  }));
+
+  const notifyStyle = useAnimatedStyle(() => ({
+    opacity: translateX.value < 0 ? 1 : 0,
+    // width: interpolate(
+    //   translateX.value,
+    //   [translateX.value + editWidth, translateX.value],
+    //   [0, 120]
+    // ),
+  }));
+
   return (
-    <Animated.View style={[styles.container, containerStyle]}>
-      <Image style={styles.picture} source={picture} />
-      <Animated.View style={[styles.textContainer, TextContainer]}>
-        <Text style={[styles.text]}>{title}</Text>
-        <Text style={[styles.text]}>{subtitle}</Text>
+    <View>
+      <Animated.View
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          backgroundColor: "red",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            return Alert.alert("aaalfono");
+          }}
+          style={{
+            width: deleteWidth,
+            alignItems: "center",
+            zIndex: 10,
+          }}
+        >
+          <Text
+            onPress={() => {
+              return Alert.alert("aaalfono");
+            }}
+            style={{ color: "white", fontWeight: "bold" }}
+          >
+            Delete
+          </Text>
+          <Button
+            onPress={() => {
+              if (onDelete) {
+                onDelete(item);
+              }
+              return Alert.alert("aaalfono");
+            }}
+            title="Delete"
+          />
+        </TouchableOpacity>
       </Animated.View>
-    </Animated.View>
+      <Animated.View
+        style={[
+          {
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor: "white",
+            justifyContent: "center",
+          },
+          notifyStyle,
+        ]}
+      >
+        <Animated.View
+          style={[
+            {
+              width: editWidth,
+              alignSelf: "flex-end",
+              alignItems: "center",
+            },
+            // notifyStyle,
+          ]}
+        >
+          <Animated.View
+            style={[
+              {
+                backgroundColor: "#06d6a0",
+                padding: 10,
+                borderRadius: 20,
+              },
+            ]}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 16,
+              }}
+            >
+              Check
+            </Text>
+          </Animated.View>
+        </Animated.View>
+      </Animated.View>
+      <PanGestureHandler
+        onGestureEvent={onGestureEvent}
+        enabled
+        activeOffsetX={[0, 5]}
+        activeOffsetY={15}
+      >
+        <Animated.View style={[styles.container, containerStyle]}>
+          <Image style={styles.picture} source={picture} />
+          <Animated.View style={[styles.textContainer, TextContainer]}>
+            <Text style={[styles.text]}>{title}</Text>
+            <Text style={[styles.text]}>{subtitle}</Text>
+          </Animated.View>
+        </Animated.View>
+      </PanGestureHandler>
+    </View>
   );
 };
 
